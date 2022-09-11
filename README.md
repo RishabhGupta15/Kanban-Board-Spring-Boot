73544f32b3764379cc8fdcabc86bb5be7415ce7d
Sumit Kumar <sumitkishore0131@gmail.com>
2022-09-11 17:11:16 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Kanban-Board-Spring-Boot
+ A kanban board an agile project management tool designed to help visualize work, limit work-in-progress.
---
+++ src/main/java/com/kanbanboard/KanbanBoardSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.kanbanboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanbanBoardSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KanbanBoardSpringBootApplication.class, args);
+    }
+
+}
